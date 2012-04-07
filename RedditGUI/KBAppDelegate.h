19442b72a4f2f59979f24a4019cf2a543b6fb6fd
Kyle Bock <kylewbock@gmail.com>
2012-04-06 20:28:20 -0400

    Initial Commit

---
+++ RedditGUI/KBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KBAppDelegate.h
+//  RedditGUI
+//
+//  Created by Kyle Bock on 4/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface KBAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ RedditGUI/KBAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  KBAppDelegate.m
+//  RedditGUI
+//
+//  Created by Kyle Bock on 4/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "KBAppDelegate.h"
+
+@implementation KBAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
